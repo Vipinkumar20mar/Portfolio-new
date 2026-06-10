c81486d8d76b6898567b5c229787ce68d0ea06d8
Vipinkumar20mar <vipin20mar@gmail.com>
2026-06-10 15:42:24 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -55,19 +55,7 @@ const About = () => {
             exploring advancements in Machine Learning and Generative AI.
           </p>
 
-          {/* Resume Button */}
-          <a
-            href="https://drive.google.com/file/d/1heQWjYV_-cG3zl1enOKbIEG3-Lq-KzTa/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
-            style={{
-              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
-              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
-            }}
-          >
-            DOWNLOAD CV
-          </a>
+          
         </div>
 
         {/* Right Side */}
